85bfcd237fea45686e4309d8e085f2e2d62266be
Aleksandr <aleksaandr24@gmail.com>
2022-06-23 21:33:18 +0500

    Add BadgeLike ui-component

--- src/components/ui/BadgeLike/BadgeLike.vue
+++ src/components/ui/BadgeLike/BadgeLike.vue
@@ -1,9 +1,39 @@
 <template>
-  <div></div>
+  <div
+    :class="className"
+    @click="addRemoveLike"
+  >
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'BadgeLike'
+  name: 'BadgeLike',
+
+  data() {
+    return {
+      isLiked: false
+    }
+  },
+
+  computed: {
+    className() {
+      let classArray = ['badge-like']
+      if (this.isLiked) {
+        classArray.push('badge-like_liked')
+      }
+      return classArray.join(' ')
+    }
+  },
+
+  methods: {
+    addRemoveLike() {
+      if (!this.isLiked) {
+        this.isLiked = true
+      } else {
+        this.isLiked = false
+      }
+    }
+  }
 }
 </script>
