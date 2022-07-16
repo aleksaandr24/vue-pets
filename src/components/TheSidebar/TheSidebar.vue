32d6089d9fa6a9acc8bce4405d7705b9810fc68c
Aleksandr <aleksaandr24@gmail.com>
2022-07-16 22:11:55 +0500

    Update TheSidebar: add menu

--- src/components/TheSidebar/TheSidebar.vue
+++ src/components/TheSidebar/TheSidebar.vue
@@ -13,13 +13,105 @@
       </router-link>
     </div>
     <nav class="sidebar__menu">
-      <div>HOM</div>
-      <div>PTS</div>
-      <div>BLG</div>
-      <div>CHT</div>
-      <div>PFL</div>
+      <ul class="menu-list">
+        <li class="menu-item">
+          <router-link to="/">
+            <img
+              v-if="!darkTheme"
+              class=""
+              src="@/assets/images/menu-home.svg"
+              alt="HOME"
+            >
+            <img
+              v-else
+              class=""
+              src="@/assets/images/menu-home-dark.svg"
+              alt="HOME"
+            >
+          </router-link>
+        </li>
+        <li class="menu-item">
+          <router-link to="/">
+            <img
+              v-if="!darkTheme"
+              class=""
+              src="@/assets/images/menu-pets.svg"
+              alt="PETS"
+            >
+            <img
+              v-else
+              class=""
+              src="@/assets/images/menu-pets-dark.svg"
+              alt="PETS"
+            >
+          </router-link>
+        </li>
+        <li class="menu-item">
+          <router-link to="/">
+            <img
+              v-if="!darkTheme"
+              class=""
+              src="@/assets/images/menu-blog.svg"
+              alt="BLOG"
+            >
+            <img
+              v-else
+              class=""
+              src="@/assets/images/menu-blog-dark.svg"
+              alt="BLOG"
+            >
+          </router-link>
+        </li>
+        <li class="menu-item">
+          <router-link to="/">
+            <img
+              v-if="!darkTheme"
+              class=""
+              src="@/assets/images/menu-chat.svg"
+              alt="CHAT"
+            >
+            <img
+              v-else
+              class=""
+              src="@/assets/images/menu-chat-dark.svg"
+              alt="CHAT"
+            >
+          </router-link>
+        </li>
+        <li class="menu-item">
+          <router-link to="/">
+            <img
+              v-if="!darkTheme"
+              class=""
+              src="@/assets/images/menu-profile.svg"
+              alt="PROF"
+            >
+            <img
+              v-else
+              class=""
+              src="@/assets/images/menu-profile-dark.svg"
+              alt="PROF"
+            >
+          </router-link>
+        </li>
+      </ul>
     </nav>
-    <div class="sidebar__logout">EXT</div>
+    <div class="sidebar__logout">
+      <router-link to="/">
+        <img
+          v-if="!darkTheme"
+          class=""
+          src="@/assets/images/logout.svg"
+          alt="PROF"
+        >
+        <img
+          v-else
+          class=""
+          src="@/assets/images/logout-dark.svg"
+          alt="PROF"
+        >
+      </router-link>
+    </div>
   </aside>
 </template>
 
